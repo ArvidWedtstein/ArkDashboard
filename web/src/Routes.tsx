f970381a66f85493e7d1e18df59bae1b2cc81db2
arvidwedtstein <bassgamer03@gmail.com>
2022-11-15 11:57:07 +0100

    private routes

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -7,7 +7,7 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Set, Router, Route } from '@redwoodjs/router'
+import { Set, Router, Route, Private } from '@redwoodjs/router'
 
 import MainLayout from 'src/layouts/MainLayout/MainLayout'
 import ScaffoldLayout from 'src/layouts/ScaffoldLayout/ScaffoldLayout'
@@ -17,18 +17,19 @@ const Routes = () => {
     <Router>
       <Set wrap={MainLayout}>
         <Set wrap={ScaffoldLayout} title="Basespots" titleTo="basespots" buttonLabel="New Basespot" buttonTo="newBasespot">
-          <Route path="/basespots/new" page={BasespotNewBasespotPage} name="newBasespot" />
-          <Route path="/basespots/{id:Int}/edit" page={BasespotEditBasespotPage} name="editBasespot" />
-          <Route path="/basespots/{id:Int}" page={BasespotBasespotPage} name="basespot" />
+          <Private unauthenticated="login">
+            <Route path="/basespots/new" page={BasespotNewBasespotPage} name="newBasespot" />
+            <Route path="/basespots/{id:Int}/edit" page={BasespotEditBasespotPage} name="editBasespot" />
+            <Route path="/basespots/{id:Int}" page={BasespotBasespotPage} name="basespot" />
+          </Private>
+
           <Route path="/basespots" page={BasespotBasespotsPage} name="basespots" />
         </Set>
         <Route path="/login" page={LoginPage} name="login" />
         <Route path="/signup" page={SignupPage} name="signup" />
         <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
         <Route path="/reset-password" page={ResetPasswordPage} name="resetPassword" />
-        {/* <Private unauthenticated="login">
-          <Route path="/posts" page={PostPostsPage} name="posts" />
-        </Private> */}
+
         <Route path="/" page={HomePage} name="home" />
 
         <Route notfound page={NotFoundPage} />
