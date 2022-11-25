3b773702d019f955a4d9046dd1f18108f28da660
arvidwedtstein <bassgamer03@gmail.com>
2022-11-25 14:02:40 +0100

    d

--- web/src/components/Basespot/Basespots/Basespots.tsx
+++ web/src/components/Basespot/Basespots/Basespots.tsx
@@ -39,7 +39,7 @@ const BasespotsList = ({ basespots }: FindBasespots) => {
 
   return (
     <div className="">
-      <div className='grid mt-8 gap-8 grid-cols-1 md:grid-cols-2 xl:grid-cols-2 mb-5'>
+      {/* <div className='grid mt-8 gap-8 grid-cols-1 md:grid-cols-2 xl:grid-cols-2 mb-5'>
         {basespots.map((basespot, i) => (
           <ArkCard
             key={i}
@@ -54,8 +54,8 @@ const BasespotsList = ({ basespots }: FindBasespots) => {
             }}
           />
         ))}
-      </div>
-      {/* <div className="grid mt-8 gap-8 grid-cols-1 md:grid-cols-2 xl:grid-cols-2 mb-5">
+      </div> */}
+      <div className="grid mt-8 gap-8 grid-cols-1 md:grid-cols-2 xl:grid-cols-2 mb-5">
         {basespots.map((basespot, i) => (
           <div
             key={i}
@@ -105,7 +105,7 @@ const BasespotsList = ({ basespots }: FindBasespots) => {
             </div>
           </div>
         ))}
-      </div> */}
+      </div>
     </div>
   )
 }
