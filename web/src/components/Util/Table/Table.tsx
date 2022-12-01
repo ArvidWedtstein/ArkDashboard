874dbdf51994e549a644dc0401768413d9b14558
ArvidWedtstein <71834553+ArvidWedtstein@users.noreply.github.com>
2022-12-01 23:41:16 +0100

    table color changed

--- web/src/components/Util/Table/Table.tsx
+++ web/src/components/Util/Table/Table.tsx
@@ -5,62 +5,80 @@ interface ITableProps<P = {}> {
   children?: React.ReactNode;
   data: Object[];
   cols?: any[];
-  renderCell?: (params: any) => React.ReactNode
+  renderCell?: (params: any) => React.ReactNode;
   tableOptions?: {
     header: boolean;
-  }
+  };
 }
-const Table = ({ data, cols, renderCell, tableOptions = { header: true } }: ITableProps) => {
+const Table = ({
+  data,
+  cols,
+  renderCell,
+  tableOptions = { header: true },
+}: ITableProps) => {
   let keys = cols || Object.keys(data[0]);
 
   // TODO: Create filtering and sorting options for table
   return (
     <div className="flex">
-      <div className="relative table table-auto my-4 w-full rounded-xl p-3 bg-white shadow">
-
-        {(tableOptions && tableOptions.header) ?? <div className="table-header-group">
-          {keys.map((key) => (
-            <div key={key} className="text-xs table-cell p-2 text-[#888da9]">{truncate(capitalize(key))}</div>
-          ))}
-        </div>}
-        {
-          data.map((row) => (
-            <div key={Math.random()} className="table-row-group">
-              {
-                keys.map((value) => (
+      <div className="relative my-4 table w-full table-auto rounded-xl bg-gray-500 p-3 shadow">
+        {(tableOptions && tableOptions.header) ?? (
+          <div className="table-header-group">
+            {keys.map((key) => (
+              <div key={key} className="table-cell p-2 text-xs text-[#888da9]">
+                {truncate(capitalize(key))}
+              </div>
+            ))}
+          </div>
+        )}
+        {data.map((row) => (
+          <div key={Math.random()} className="table-row-group">
+            {keys.map((value) => (
+              <>
+                {value == "actions" ? (
+                  <div
+                    key={value}
+                    className="table-cell p-2 text-xs text-black"
+                  >
+                    <nav className="flex flex-row">
+                      <Link
+                        to={routes.tribe({ id: row["id"] })}
+                        title={"Show detail"}
+                        className="rw-button rw-button-small"
+                      >
+                        Show
+                      </Link>
+                    </nav>
+                  </div>
+                ) : (
                   <>
-                    {value == "actions" ? (
-                      <div key={value} className="table-cell text-xs text-black p-2">
-                        <nav className="flex flex-row">
-                          <Link
-                            to={routes.tribe({ id: row["id"] })}
-                            title={'Show detail'}
-                            className="rw-button rw-button-small"
-                          >
-                            Show
-                          </Link>
-                        </nav>
+                    {renderCell ? (
+                      <div
+                        key={Math.random()}
+                        className="table-cell p-2 text-xs text-black"
+                      >
+                        {renderCell({ id: value, amount: row[value] })}
                       </div>
                     ) : (
-                      <>
-                        {
-                          renderCell ? (
-                            <div key={Math.random()} className="table-cell text-xs text-black p-2">{renderCell({ id: value, amount: row[value] })}</div>
-                          ) : (
-                            <div key={Math.random()} className="table-cell text-xs text-black p-2"> {isDate(row[value]) ? timeTag(row[value]) : truncate(row[value])}</div>
-                          )
-                        }
-                      </>
+                      <div
+                        key={Math.random()}
+                        className="table-cell p-2 text-xs text-black"
+                      >
+                        {" "}
+                        {isDate(row[value])
+                          ? timeTag(row[value])
+                          : truncate(row[value])}
+                      </div>
                     )}
                   </>
-                ))
-              }
-            </div>
-          ))
-        }
+                )}
+              </>
+            ))}
+          </div>
+        ))}
       </div>
-    </div >
-  )
-}
+    </div>
+  );
+};
 
-export default Table
+export default Table;
