95269922a2fe99ceb5fbddd32bf12a7429f64649
arvidwedtstein <bassgamer03@gmail.com>
2022-12-05 15:02:20 +0100

    c

--- web/src/components/Basespot/Basespot/Basespot.tsx
+++ web/src/components/Basespot/Basespot/Basespot.tsx
@@ -59,36 +59,22 @@ const Basespot = ({ basespot }: Props) => {
     }
   };
 
-  const scrollFunction = () => {
-    let scrolled = window.scrollY;
-    console.log(scrolled)
-  }
-  window.onscroll = function () {
-    scrollFunction();
-  };
+  // const scrollFunction = () => {
+  //   let scrolled = window.scrollY;
+  //   console.log(scrolled)
+  // }
+  // window.onscroll = function () {
+  //   scrollFunction();
+  // };
   return (
     <>
       {/* TODO: Maek better layout */}
-      <div className='flex items-center justify-center mb-3'>
-        <div className='w-full max-w-lg px-10 py-8 mx-auto bg-white rounded-lg shadow-xl'>
-          <div className='max-w-md mx-auto space-y-6'>
-
-            <h2 className="flex flex-row flex-nowrap items-center my-8">
-              <span className="flex-grow block border-t border-black" aria-hidden="true" role="presentation"></span>
-              <span className="flex-none block mx-4   px-4 py-2.5 text-xs leading-none font-medium uppercase bg-black text-white">
-                {basespot.name}
-              </span>
-              <span className="flex-grow block border-t border-black" aria-hidden="true" role="presentation"></span>
-            </h2>
-
-          </div>
-        </div>
-      </div>
+      {/* TODO: Make map required not optional */}
       <section className="text-gray-700 body-font">
-        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+        <div className="container mx-auto flex px-5 py-12 md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Before they sold out
-              <br className="hidden lg:inline-block" />readymade gluten
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">{basespot.name}
+              <br className="hidden lg:inline-block" />{basespot.Map.split(/(?=[A-Z])/).join(" ")}
             </h1>
             <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
             <div className="flex justify-center">
