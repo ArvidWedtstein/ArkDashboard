236a4ace1d563f40d58bd12f4254db205f465b31
arvidwedtstein <bassgamer03@gmail.com>
2022-12-05 13:21:14 +0100

    basespot hero

--- web/src/components/Basespot/Basespot/Basespot.tsx
+++ web/src/components/Basespot/Basespot/Basespot.tsx
@@ -59,9 +59,30 @@ const Basespot = ({ basespot }: Props) => {
     }
   };
 
+  const scrollFunction = () => {
+    let scrolled = window.scrollY;
+  }
+  window.onscroll = function () {
+    scrollFunction();
+  };
   return (
     <>
       {/* TODO: Maek better layout */}
+      <div className='flex items-center justify-center min-h-screen' style={{ background: `url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvC4tJUjp6TudN0t7kMxrGll3AQDUOPCncWSSogN5lgA&s')`, backgroundSize: "cover", backgroundPosition: "center" }}>
+        <div className='w-full max-w-lg px-10 py-8 mx-auto bg-white rounded-lg shadow-xl'>
+          <div className='max-w-md mx-auto space-y-6'>
+
+            <h2 className="flex flex-row flex-nowrap items-center my-8">
+              <span className="flex-grow block border-t border-black" aria-hidden="true" role="presentation"></span>
+              <span className="flex-none block mx-4   px-4 py-2.5 text-xs leading-none font-medium uppercase bg-black text-white">
+                Heading Text
+              </span>
+              <span className="flex-grow block border-t border-black" aria-hidden="true" role="presentation"></span>
+            </h2>
+
+          </div>
+        </div>
+      </div>
       <div className="grid auto-cols-auto grid-cols-2">
         <div className="">
           <img
