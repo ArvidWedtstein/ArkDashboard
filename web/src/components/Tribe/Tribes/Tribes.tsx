a7cc1ae728920e5dcecfad73bfdb378c82d3f88e
arvidwedtstein <bassgamer03@gmail.com>
2022-12-05 11:26:23 +0100

    d

--- web/src/components/Tribe/Tribes/Tribes.tsx
+++ web/src/components/Tribe/Tribes/Tribes.tsx
@@ -65,11 +65,11 @@ const TribesList = ({ tribes }: FindTribes) => {
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
-              stroke-width="2"
+              strokeWidth="2"
             >
               <path
                 strokeLinecap="round"
-                stroke-linejoin="round"
+                strokeLinejoin="round"
                 d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
               />
             </svg>
