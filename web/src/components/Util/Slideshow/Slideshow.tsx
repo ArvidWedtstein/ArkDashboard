6f4662ddd5b1ac50b3e92ca3c79c5c228e0fa1a4
arvidwedtstein <bassgamer03@gmail.com>
2022-12-07 09:46:39 +0100

    r

--- web/src/components/Util/Slideshow/Slideshow.tsx
+++ web/src/components/Util/Slideshow/Slideshow.tsx
@@ -1,8 +1,11 @@
 interface ISlideshowProps {
-  images?: string[];
+  slides?: {
+    url: string;
+    title: string;
+    subtitle: string;
+  }[];
 }
-const Slideshow = ({ images }: ISlideshowProps) => {
-  const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "#FF0000"];
+const Slideshow = ({ slides }: ISlideshowProps) => {
   const [index, setIndex] = React.useState(0);
   const timeoutRef = React.useRef(null);
   const delay = 5000;
@@ -18,7 +21,7 @@ const Slideshow = ({ images }: ISlideshowProps) => {
     timeoutRef.current = setTimeout(
       () =>
         setIndex((prevIndex) =>
-          prevIndex === colors.length - 1 ? 0 : prevIndex + 1
+          prevIndex === slides.length - 1 ? 0 : prevIndex + 1
         ),
       delay
     );
@@ -32,14 +35,14 @@ const Slideshow = ({ images }: ISlideshowProps) => {
     <div className="my-0 mx-auto overflow-hidden max-w-[500px]">
       <div className="whitespace-nowrap transition ease-in-out duration-500"
         style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}>
-        {images.map((url, index) => (
+        {slides.map((slide, index) => (
           <div className="w-full h-[400px] rounded inline-block" key={index}>
-            {images && <img src={url} className="w-full h-full object-cover" />}
+            {slide && <img src={slide.url} className="w-full h-full object-cover" />}
           </div>
         ))}
       </div>
       <div className="text-center">
-        {colors.map((_, idx) => (
+        {slides.map((_, idx) => (
           <div
             key={idx}
             className={`inline-block h-5 w-5 rounded-full cursor-pointer mt-4 px-2 ${index === idx ? "bg-green-700" : "bg-[#c4c4c4]"}`}
