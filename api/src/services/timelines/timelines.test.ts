dbf797ae290719442b787e021fe7c8eab4d54e19
ArvidWedtstein <71834553+ArvidWedtstein@users.noreply.github.com>
2023-01-04 18:09:42 +0100

    timeline sdl

---
+++ api/src/services/timelines/timelines.test.ts
@@ -0,0 +1,39 @@
+import type { timeline } from '@prisma/client'
+
+import {
+  timelines,
+  timeline,
+  createTimeline,
+  updateTimeline,
+  deleteTimeline,
+} from './timelines'
+import type { StandardScenario } from './timelines.scenarios'
+
+// Generated boilerplate tests do not account for all circumstances
+// and can fail without adjustments, e.g. Float.
+//           Please refer to the RedwoodJS Testing Docs:
+//       https://redwoodjs.com/docs/testing#testing-services
+// https://redwoodjs.com/docs/testing#jest-expect-type-considerations
+
+describe('timelines', () => {
+  scenario('returns all timelines', async (scenario: StandardScenario) => {
+    const result = await timelines()
+
+    expect(result.length).toEqual(Object.keys(scenario.timeline).length)
+  })
+
+  scenario('returns a single timeline', async (scenario: StandardScenario) => {
+    const result = await timeline({ id: scenario.timeline.one.id })
+
+    expect(result).toEqual(scenario.timeline.one)
+  })
+
+  scenario('deletes a timeline', async (scenario: StandardScenario) => {
+    const original = (await deleteTimeline({
+      id: scenario.timeline.one.id,
+    })) as timeline
+    const result = await timeline({ id: original.id })
+
+    expect(result).toEqual(null)
+  })
+})
---
+++ api/src/services/timelines/timelines.ts
@@ -0,0 +1,52 @@
+import type {
+  QueryResolvers,
+  MutationResolvers,
+  TimelineRelationResolvers,
+} from 'types/graphql'
+
+import { db } from 'src/lib/db'
+
+export const timelines: QueryResolvers['timelines'] = () => {
+  return db.timeline.findMany()
+}
+
+export const timeline: QueryResolvers['timeline'] = ({ id }) => {
+  return db.timeline.findUnique({
+    where: { id },
+  })
+}
+
+export const createTimeline: MutationResolvers['createTimeline'] = ({
+  input,
+}) => {
+  return db.timeline.create({
+    data: input,
+  })
+}
+
+export const updateTimeline: MutationResolvers['updateTimeline'] = ({
+  id,
+  input,
+}) => {
+  return db.timeline.update({
+    data: input,
+    where: { id },
+  })
+}
+
+export const deleteTimeline: MutationResolvers['deleteTimeline'] = ({ id }) => {
+  return db.timeline.delete({
+    where: { id },
+  })
+}
+
+export const Timeline: TimelineRelationResolvers = {
+  TimelineBasespot: (_obj, { root }) => {
+    return db.timeline
+      .findUnique({ where: { id: root?.id } })
+      .TimelineBasespot()
+  },
+  profiles: (_obj, { root }) => {
+    return db.timeline.findUnique({ where: { id: root?.id } }).profiles()
+  },
+}
