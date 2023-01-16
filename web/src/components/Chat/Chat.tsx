93c49ff859edf402d5acddb54f6e6a86702347fe
arvidwedtstein <bassgamer03@gmail.com>
2023-01-16 08:24:05 +0100

    todo

--- web/src/components/Chat/Chat.tsx
+++ web/src/components/Chat/Chat.tsx
@@ -54,6 +54,7 @@ const Message = ({ message, profile, setProfileCache }: { message: IMessage, pro
   return (
     <div key={message.id} aria-owns={message.profile_id === userId ? 'owner' : ''} className="flex pt-0 px-5 pb-11 aria-[owns=owner]:flex-row-reverse group chat-msg owner">
       <div className="flex-shrink-0 mt-auto -mb-5 relative chat-msg-profile">
+        {/* TODO: Replace with avatar component later */}
         <img className="h-10 w-10 rounded-full object-cover" src={profile.avatar_url || "https://s3-us-west-2.amazonaws.com/s.cdpn.io/3364143/download+%281%29.png"} alt={profile.username} title={profile.username} />
         <div className="absolute bottom-0 text-xs font-semibold whitespace-nowrap left-[calc(100%+12px)] text-[#626466] group-aria-[owns=owner]:left-auto group-aria-[owns=owner]:right-[calc(100%+12px)] chat-msg-date">{timeTag(message.created_at)}</div>
       </div>
