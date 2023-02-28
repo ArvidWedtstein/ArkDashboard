25b732a97d55a3f52d33bfcd44bb62f88ad5e362
arvidwedtstein <bassgamer03@gmail.com>
2023-02-28 14:44:40 +0100

    - added weight icon

--- web/src/components/Item/Item/Item.tsx
+++ web/src/components/Item/Item/Item.tsx
@@ -76,8 +76,10 @@ const Item = ({ item }: Props) => {
               {gathereffvals.map((eff) => (
                 <div className="flex items-center">
                   <p className="text-sm mr-2 w-20">{eff.name}</p>
-
-                  <p className="text-lime-300 mr-1">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="inline-block fill-current w-4">
+                    <path d="M510.3 445.9L437.3 153.8C433.5 138.5 420.8 128 406.4 128H346.1c3.625-9.1 5.875-20.75 5.875-32c0-53-42.1-96-96-96S159.1 43 159.1 96c0 11.25 2.25 22 5.875 32H105.6c-14.38 0-27.13 10.5-30.88 25.75l-73.01 292.1C-6.641 479.1 16.36 512 47.99 512h416C495.6 512 518.6 479.1 510.3 445.9zM256 128C238.4 128 223.1 113.6 223.1 96S238.4 64 256 64c17.63 0 32 14.38 32 32S273.6 128 256 128z" />
+                  </svg>
+                  <p className="text-lime-300 mx-1">
                     50%
                   </p>
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" className="text-lime-300 inline-block fill-current w-4">
