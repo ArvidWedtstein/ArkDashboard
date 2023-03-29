1e0876c98397eb8fd5b638385caee2db43f5547c
ArvidWedtstein <71834553+ArvidWedtstein@users.noreply.github.com>
2023-03-29 20:26:32 +0200

    - added lootcrate sdl

---
+++ api/src/services/lootcrates/lootcrates.test.ts
@@ -0,0 +1,60 @@
+import type { Lootcrate } from '@prisma/client'
+
+import {
+  lootcrates,
+  lootcrate,
+  createLootcrate,
+  updateLootcrate,
+  deleteLootcrate,
+} from './lootcrates'
+import type { StandardScenario } from './lootcrates.scenarios'
+
+// Generated boilerplate tests do not account for all circumstances
+// and can fail without adjustments, e.g. Float.
+//           Please refer to the RedwoodJS Testing Docs:
+//       https://redwoodjs.com/docs/testing#testing-services
+// https://redwoodjs.com/docs/testing#jest-expect-type-considerations
+
+describe('lootcrates', () => {
+  scenario('returns all lootcrates', async (scenario: StandardScenario) => {
+    const result = await lootcrates()
+
+    expect(result.length).toEqual(Object.keys(scenario.lootcrate).length)
+  })
+
+  scenario('returns a single lootcrate', async (scenario: StandardScenario) => {
+    const result = await lootcrate({ id: scenario.lootcrate.one.id })
+
+    expect(result).toEqual(scenario.lootcrate.one)
+  })
+
+  scenario('creates a lootcrate', async (scenario: StandardScenario) => {
+    const result = await createLootcrate({
+      input: { blueprint: 'String', map: scenario.lootcrate.two.map },
+    })
+
+    expect(result.blueprint).toEqual('String')
+    expect(result.map).toEqual(scenario.lootcrate.two.map)
+  })
+
+  scenario('updates a lootcrate', async (scenario: StandardScenario) => {
+    const original = (await lootcrate({
+      id: scenario.lootcrate.one.id,
+    })) as Lootcrate
+    const result = await updateLootcrate({
+      id: original.id,
+      input: { blueprint: 'String2' },
+    })
+
+    expect(result.blueprint).toEqual('String2')
+  })
+
+  scenario('deletes a lootcrate', async (scenario: StandardScenario) => {
+    const original = (await deleteLootcrate({
+      id: scenario.lootcrate.one.id,
+    })) as Lootcrate
+    const result = await lootcrate({ id: original.id })
+
+    expect(result).toEqual(null)
+  })
+})
