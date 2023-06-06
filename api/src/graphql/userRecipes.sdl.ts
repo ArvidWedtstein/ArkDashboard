1a43b715f5f6152b2c1b47bd3e8c4a3dae6d6ec1
arvidwedtstein <bassgamer03@gmail.com>
2023-06-06 14:50:21 +0200

    - reqAuth fix

--- api/src/graphql/userRecipes.sdl.ts
+++ api/src/graphql/userRecipes.sdl.ts
@@ -13,7 +13,7 @@ export const schema = gql`
   type Query {
     userRecipes: [UserRecipe!]! @requireAuth
     userRecipe(id: String!): UserRecipe @requireAuth
-    userRecipesByID(user_id: String): [UserRecipe!]! @requireAuth
+    userRecipesByID(user_id: String): [UserRecipe!]! @skipAuth
   }
 
   input CreateUserRecipeInput {
