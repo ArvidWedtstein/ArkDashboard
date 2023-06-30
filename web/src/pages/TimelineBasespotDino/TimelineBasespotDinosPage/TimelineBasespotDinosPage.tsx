49213453bc5c56dada54967563fec75ec1cbbb32
ArvidWedtstein <71834553+ArvidWedtstein@users.noreply.github.com>
2023-06-30 18:32:30 +0200

    - removed timelinebasespotdino

--- web/src/pages/TimelineBasespotDino/TimelineBasespotDinosPage/TimelineBasespotDinosPage.tsx
+++
@@ -1,7 +0,0 @@
-import TimelineBasespotDinosCell from 'src/components/TimelineBasespotDino/TimelineBasespotDinosCell'
-
-const TimelineBasespotDinosPage = () => {
-  return <TimelineBasespotDinosCell />
-}
-
-export default TimelineBasespotDinosPage
