61a4e218f9f39fdaa75e6a811145a0d8dfa5b039
arvidwedtstein <bassgamer03@gmail.com>
2023-07-06 15:08:25 +0200

    - admin

--- web/src/components/Admin/Admin/Admin.tsx
+++ web/src/components/Admin/Admin/Admin.tsx
@@ -63,6 +63,57 @@ const Admin = ({ basespots }: FindAdminData) => {
           <StatCard stat={"Test"} value={10} />
           <StatCard stat={"Test"} value={10} />
         </div>
+        <svg id="SvgjsSvg1808" width="531" height="95" xmlns="http://www.w3.org/2000/svg" version="1.1" className="apexcharts-svg" transform="translate(0, 0)">
+          <foreignObject x="0" y="0" width="531" height="95">
+            <div className="apexcharts-legend"></div>
+          </foreignObject>
+          <rect id="SvgjsRect1812" width="0" height="0" x="0" y="0" rx="0" ry="0" opacity="1" stroke-width="0" stroke="none" stroke-dasharray="0" fill="#fefefe"></rect>
+          <g id="SvgjsG1849" className="apexcharts-yaxis" transform="translate(-18, 0)"></g>
+          <g id="SvgjsG1810" className="apexcharts-inner apexcharts-graphical" transform="translate(0, 0)">
+            <defs id="SvgjsDefs1809">
+              <clipPath id="gridRectMaskf22gi0fs">
+                <rect id="SvgjsRect1814" width="536" height="96" x="-2.5" y="-0.5" rx="0" ry="0" opacity="1" stroke-width="0" stroke="none" stroke-dasharray="0" fill="#fff"></rect>
+              </clipPath>
+              <clipPath id="forecastMaskf22gi0fs"></clipPath>
+              <clipPath id="nonForecastMaskf22gi0fs"></clipPath>
+              <clipPath id="gridRectMarkerMaskf22gi0fs">
+                <rect id="SvgjsRect1815" width="535" height="99" x="-2" y="-2" rx="0" ry="0" opacity="1" stroke-width="0" stroke="none" stroke-dasharray="0" fill="#fff"></rect>
+              </clipPath>
+              <linearGradient id="SvgjsLinearGradient1820" x1="0" y1="0" x2="0" y2="1">
+                <stop id="SvgjsStop1821" stop-opacity="0.65" stop-color="rgba(69,39,160,0.65)" offset="0"></stop>
+                <stop id="SvgjsStop1822" stop-opacity="0.5" stop-color="rgba(162,147,208,0.5)" offset="1"></stop>
+                <stop id="SvgjsStop1823" stop-opacity="0.5" stop-color="rgba(162,147,208,0.5)" offset="1"></stop>
+              </linearGradient>
+            </defs>
+            <g id="SvgjsG1826" className="apexcharts-grid">
+              {/* Helper lines */}
+              {/* <g id="SvgjsG1827" className="apexcharts-gridlines-horizontal" style={{}}>
+                <line id="SvgjsLine1830" x1="0" y1="0" x2="531" y2="0" stroke="#e0e0e0" stroke-dasharray="0" stroke-linecap="butt" className="apexcharts-gridline"></line>
+                <line id="SvgjsLine1831" x1="0" y1="19" x2="531" y2="19" stroke="#e0e0e0" stroke-dasharray="0" stroke-linecap="butt" className="apexcharts-gridline"></line>
+                <line id="SvgjsLine1832" x1="0" y1="38" x2="531" y2="38" stroke="#e0e0e0" stroke-dasharray="0" stroke-linecap="butt" className="apexcharts-gridline"></line>
+                <line id="SvgjsLine1833" x1="0" y1="57" x2="531" y2="57" stroke="#e0e0e0" stroke-dasharray="0" stroke-linecap="butt" className="apexcharts-gridline"></line>
+                <line id="SvgjsLine1834" x1="0" y1="76" x2="531" y2="76" stroke="#e0e0e0" stroke-dasharray="0" stroke-linecap="butt" className="apexcharts-gridline"></line>
+                <line id="SvgjsLine1835" x1="0" y1="95" x2="531" y2="95" stroke="#e0e0e0" stroke-dasharray="0" stroke-linecap="butt" className="apexcharts-gridline"></line>
+              </g> */}
+              <g id="SvgjsG1828" className="apexcharts-gridlines-vertical" style={{ display: 'none' }}></g>
+              <line id="SvgjsLine1837" x1="0" y1="95" x2="531" y2="95" stroke="transparent" stroke-dasharray="0" stroke-linecap="butt"></line>
+              <line id="SvgjsLine1836" x1="0" y1="1" x2="0" y2="95" stroke="transparent" stroke-dasharray="0" stroke-linecap="butt"></line>
+            </g>
+            <g id="SvgjsG1816" className="apexcharts-area-series apexcharts-plot-series"><g id="SvgjsG1817" className="apexcharts-series">
+              <path id="SvgjsPath1824" d="M 0 95 L 0 95C 30.974999999999998 95 57.525000000000006 66.5 88.5 66.5C 119.475 66.5 146.025 76 177 76C 207.975 76 234.525 0 265.5 0C 296.475 0 323.025 38 354 38C 384.975 38 411.525 19 442.5 19C 473.475 19 500.025 47.5 531 47.5C 531 47.5 531 47.5 531 95M 531 47.5z" fill="url(#SvgjsLinearGradient1820)" fill-opacity="1" stroke-opacity="1" stroke-linecap="butt" stroke-width="0" stroke-dasharray="0" className="apexcharts-area" clip-path="url(#gridRectMaskf22gi0fs)" path="M 0 95 L 0 95C 30.974999999999998 95 57.525000000000006 66.5 88.5 66.5C 119.475 66.5 146.025 76 177 76C 207.975 76 234.525 0 265.5 0C 296.475 0 323.025 38 354 38C 384.975 38 411.525 19 442.5 19C 473.475 19 500.025 47.5 531 47.5C 531 47.5 531 47.5 531 95M 531 47.5z" ></path>
+              <path id="SvgjsPath1825" d="M 0 95C 30.974999999999998 95 57.525000000000006 66.5 88.5 66.5C 119.475 66.5 146.025 76 177 76C 207.975 76 234.525 0 265.5 0C 296.475 0 323.025 38 354 38C 384.975 38 411.525 19 442.5 19C 473.475 19 500.025 47.5 531 47.5" fill="none" fill-opacity="1" stroke="#4527a0" stroke-opacity="1" stroke-linecap="butt" stroke-width="1" stroke-dasharray="0" className="apexcharts-area" clip-path="url(#gridRectMaskf22gi0fs)" path="M 0 95C 30.974999999999998 95 57.525000000000006 66.5 88.5 66.5C 119.475 66.5 146.025 76 177 76C 207.975 76 234.525 0 265.5 0C 296.475 0 323.025 38 354 38C 384.975 38 411.525 19 442.5 19C 473.475 19 500.025 47.5 531 47.5" fill-rule="evenodd"></path>
+              <g id="SvgjsG1818" className="apexcharts-series-markers-wrap">
+                <g className="apexcharts-series-markers"><circle id="SvgjsCircle1853" r="0" cx="265.5" cy="0" className="apexcharts-marker wv69qqmnq no-pointer-events" stroke="#ffffff" fill="#4527a0" fill-opacity="1" stroke-width="2" stroke-opacity="0.9" default-marker-size="0"></circle></g></g></g>
+              <g id="SvgjsG1819" className="apexcharts-datalabels"></g>
+            </g>
+            <g id="SvgjsG1829" className="apexcharts-grid-borders" style={{ display: 'none' }}></g>
+            <line id="SvgjsLine1838" x1="0" y1="0" x2="531" y2="0" stroke="#b6b6b6" stroke-dasharray="0" stroke-width="1" stroke-linecap="butt" className="apexcharts-ycrosshairs"></line>
+            <line id="SvgjsLine1839" x1="0" y1="0" x2="531" y2="0" stroke-dasharray="0" stroke-width="0" stroke-linecap="butt" className="apexcharts-ycrosshairs-hidden"></line>
+            <g id="SvgjsG1840" className="apexcharts-xaxis" transform="translate(0, 0)">
+              <g id="SvgjsG1841" className="apexcharts-xaxis-texts-g" transform="translate(0, -4)"></g>
+            </g>
+          </g>
+        </svg>
         <Table rows={optimizedBasespots} settings={{
           select: true,
           pagination: {
