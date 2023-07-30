2a9cd4ae5ff49396d3a6618798d4f45470a8fa19
ArvidWedtstein <71834553+ArvidWedtstein@users.noreply.github.com>
2023-07-30 23:59:15 +0200

    - fileupload image thumbnail radio button added

--- web/src/components/Util/FileUpload/FileUpload.tsx
+++ web/src/components/Util/FileUpload/FileUpload.tsx
@@ -10,6 +10,7 @@ interface IFileUploadProps {
   storagePath: string;
   sizeLimit?: number;
   name?: string;
+  thumbnail?: boolean;
 }
 const FileUpload = ({
   onUpload,
@@ -17,10 +18,14 @@ const FileUpload = ({
   sizeLimit,
   name,
   multiple,
+  thumbnail = false,
 }: IFileUploadProps) => {
   let filename = "";
   let id = Math.round(Math.random() * 100).toString();
-  const [files, setFiles] = useState<{ file: any, imagePreviewUrl: string | ArrayBuffer }[]>([]);
+  const [files, setFiles] = useState<
+    { file: any; imagePreviewUrl: string | ArrayBuffer; thumbnail?: boolean }[]
+  >([]);
+
   const [state, setState] = useState(0);
   const [progress, setProgress] = useState(0);
   const [imagePreview, setImagePreview] = useState(null);
@@ -39,14 +44,20 @@ const FileUpload = ({
   }, []);
 
   const fileHandle = (e) => {
-
     stateDisplay();
     return new Promise(() => {
       const { target } = e;
       if (target?.files.length) {
         let reader = new FileReader();
         reader.onload = (e2) => {
-          setFiles([...files, ...Array.from(target.files).map((file) => ({ file, imagePreviewUrl: reader.result }))]);
+          setFiles([
+            ...files,
+            ...Array.from(target.files).map((file) => ({
+              file,
+              imagePreviewUrl: reader.result,
+              thumbnail: false,
+            })),
+          ]);
           fileDisplay(
             files.length > 1 ? `${files.length} files` : target.files[0].name
           );
@@ -54,16 +65,25 @@ const FileUpload = ({
         reader.readAsDataURL(target.files[0]);
       }
     });
-  }
+  };
   const handleDrop = (event) => {
     event.preventDefault();
     stateDisplay();
     let reader = new FileReader();
     reader.onload = (e2) => {
       reader.result;
-      setFiles((prev) => [...prev, ...Array.from(event.dataTransfer.files).map((file) => ({ file, imagePreviewUrl: reader.result }))]);
+      setFiles((prev) => [
+        ...prev,
+        ...Array.from(event.dataTransfer.files).map((file) => ({
+          file,
+          imagePreviewUrl: reader.result,
+          thumbnail: false,
+        })),
+      ]);
       fileDisplay(
-        files.length > 1 ? `${files.length} files` : event.dataTransfer.files[0].name
+        files.length > 1
+          ? `${files.length} files`
+          : event.dataTransfer.files[0].name
       );
     };
     reader.readAsDataURL(event.dataTransfer.files[0]);
@@ -75,7 +95,7 @@ const FileUpload = ({
 
   const stateDisplay = () => {
     elRef.current.setAttribute("data-state", `${state}`);
-  }
+  };
 
   const fileDisplay = (name = "") => {
     // update the name
@@ -83,7 +103,7 @@ const FileUpload = ({
 
     // show the file
     elRef?.current.setAttribute("data-ready", filename ? "true" : "false");
-  }
+  };
   const cancel = () => {
     isUploading = false;
     setProgress(0);
@@ -96,27 +116,27 @@ const FileUpload = ({
 
   const fileReset = (index = 0) => {
     setFiles((prev) => prev.filter((file) => file !== files[index]));
-    fileDisplay(
-      files.length > 1 ? `${files.length} files` : ""
-    );
+    fileDisplay(files.length > 1 ? `${files.length} files` : "");
     stateDisplay();
-  }
+  };
 
   const progressDisplay = () => {
     const progressValue = elRef?.current.querySelector("[data-progress-value]");
-    const progressFill: HTMLElement = elRef?.current.querySelector("[data-progress-fill]");
+    const progressFill: HTMLElement = elRef?.current.querySelector(
+      "[data-progress-fill]"
+    );
     const progressTimes100 = Math.floor(progress * 100);
 
     if (progressValue) progressValue.textContent = `${progressTimes100}%`;
     if (progressFill)
       progressFill.style.transform = `translateX(${progressTimes100}%)`;
-  }
+  };
 
   const file = () => {
     let t: HTMLInputElement = elRef?.current.querySelector(`#fileupload-${id}`);
     t.click();
     stateDisplay();
-  }
+  };
 
   const upload = () => {
     if (!isUploading) {
@@ -151,14 +171,14 @@ const FileUpload = ({
       }
     }
     stateDisplay();
-  }
+  };
   const fail = () => {
     isUploading = false;
     setProgress(0);
     progressTimeout = null;
     setState(2);
     stateDisplay();
-  }
+  };
 
   const progressLoop = async () => {
     progressDisplay();
@@ -189,16 +209,18 @@ const FileUpload = ({
     } catch (error) {
       fail();
     }
-  }
+  };
 
   const success = () => {
     isUploading = false;
     setState(3);
     stateDisplay();
-  }
+  };
 
   const copy = async () => {
-    const copyButton: HTMLButtonElement = elRef?.current.querySelector("[data-action='copy']");
+    const copyButton: HTMLButtonElement = elRef?.current.querySelector(
+      "[data-action='copy']"
+    );
 
     if (!isCopying && copyButton) {
       // disable
@@ -214,7 +236,7 @@ const FileUpload = ({
       copyButton.disabled = false;
       copyButton.textContent = "Copy Link";
     }
-  }
+  };
 
   return (
     <div
@@ -337,7 +359,8 @@ const FileUpload = ({
               Upload {multiple ? "Files" : "a File"}
             </h2>
             <p className="mb-6 min-h-[3rem] text-center text-base">
-              Select a file to upload from your computer or device<br />
+              Select a file to upload from your computer or device
+              <br />
               or drag and drop a file here.
             </p>
             <div className="mb-2 flex flex-wrap items-center delay-200">
@@ -354,9 +377,11 @@ const FileUpload = ({
                 multiple={multiple}
                 onChange={fileHandle}
                 type="file"
+                accept="image/png, image/jpeg"
                 hidden
               />
             </div>
+            {/* File list */}
             <div
               className={`flex-wrap items-center delay-200 group-data-[ready=true]:flex group-data-[ready=false]:hidden`}
             >
@@ -365,7 +390,12 @@ const FileUpload = ({
                   className="flex w-full flex-row items-center justify-start space-x-3"
                   key={`file-${index}`}
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" className="h-6 w-6 text-[#737a8c] transition-colors" fill="currentColor">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 384 512"
+                    className="h-5 w-5 text-white/60 transition-colors"
+                    fill="currentColor"
+                  >
                     {file.type.includes("image") ? (
                       <path d="M190.3 285.7l-26.36 40.67c-12-14.92-37.75-13.73-48.31 2.531l-46.69 72.02c-5.984 9.266-6.531 21.09-1.453 30.84C72.67 441.8 82.83 448 93.1 448h196c11.17 0 21.33-6.219 26.55-16.23c5.094-9.797 4.531-21.62-1.484-30.86l-74.66-115.2C229.3 268.5 201.4 268.5 190.3 285.7zM286.7 416L95.77 416l44.89-66.95l9.922 15.3c5.906 9.094 20.97 9.094 26.84 0l37.91-58.48L286.7 416zM96 280c13.25 0 24-10.75 24-24c0-13.26-10.75-24-24-24S72 242.7 72 256C72 269.3 82.75 280 96 280zM365.3 125.3l-106.5-106.5C246.7 6.742 230.5 0 213.5 0H64C28.65 0 0 28.65 0 64l.0065 384c0 35.35 28.65 64 64 64H320c35.35 0 64-28.65 64-64V170.5C384 153.5 377.3 137.3 365.3 125.3zM224 34.08c4.477 1.566 8.666 3.846 12.12 7.299l106.5 106.5C346.1 151.3 348.4 155.5 349.9 160H240C231.2 160 224 152.8 224 144V34.08zM352 448c0 17.64-14.36 32-32 32H64c-17.64 0-32-14.36-32-32V64c0-17.64 14.36-32 32-32h128v112C192 170.5 213.5 192 240 192H352V448z" />
                     ) : (
@@ -373,7 +403,7 @@ const FileUpload = ({
                     )}
                   </svg>
 
-                  <div className="text-left text-ellipsis whitespace-nowrap text-xs flex-grow">
+                  <div className="flex-grow text-ellipsis whitespace-nowrap text-left text-xs">
                     {file.name}
                   </div>
 
@@ -384,6 +414,23 @@ const FileUpload = ({
                   >
                     Preview
                   </button>
+
+                  {thumbnail && (
+                    <input
+                      type="radio"
+                      className="rw-input rw-input-small"
+                      title="thumbnail"
+                      name="thumbnail"
+                      checked={files[index].thumbnail}
+                      value={files[index].file.name}
+                      onChange={(e) => {
+                        setFiles((prev) => {
+                          prev[index].thumbnail = e.target.checked;
+                          return prev;
+                        });
+                      }}
+                    />
+                  )}
 
                   <button
                     className="hover:text-red-500"
@@ -408,17 +455,14 @@ const FileUpload = ({
                         <polyline points="12,4 4,12" />
                       </g>
                     </svg>
-                    <span className="sr-only">
-                      Remove
-                    </span>
+                    <span className="sr-only">Remove</span>
                   </button>
                 </div>
               ))}
             </div>
             {files.length > 0 && (
-
               <button
-                className="mt-3 rw-button rw-button-gray-outline w-full rw-button-medium"
+                className="rw-button rw-button-gray-outline rw-button-medium mt-3 w-full"
                 type="button"
                 onClick={upload}
               >
@@ -426,7 +470,7 @@ const FileUpload = ({
               </button>
             )}
             {imagePreview && (
-              <div className="mt-3 bg-slate-700 p-4 pt-2">
+              <div className="mt-3 rounded-lg bg-zinc-700 p-2">
                 <button
                   className="rw-button rw-button-small rw-button-red float-right my-2"
                   type="button"
