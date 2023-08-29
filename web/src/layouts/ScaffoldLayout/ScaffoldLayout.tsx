72fa2c9606929d57d326a715a8b55362fcde5be9
ArvidWedtstein <71834553+ArvidWedtstein@users.noreply.github.com>
2023-08-29 23:42:22 +0200

    - updated table diagram.

--- web/src/layouts/ScaffoldLayout/ScaffoldLayout.tsx
+++ web/src/layouts/ScaffoldLayout/ScaffoldLayout.tsx
@@ -1,37 +1,17 @@
-import { Link, routes } from '@redwoodjs/router'
-import { Toaster } from '@redwoodjs/web/toast'
+import { Link, routes } from "@redwoodjs/router";
+import { Toaster } from "@redwoodjs/web/toast";
 
 type LayoutProps = {
-  title: string
-  titleTo: string
-  buttonLabel: string
-  buttonTo: string
-  children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 
-const ScaffoldLayout = ({
-  title,
-  titleTo,
-  buttonLabel,
-  buttonTo,
-  children,
-}: LayoutProps) => {
+const ScaffoldLayout = ({ children }: LayoutProps) => {
   return (
-    <div className="rw-scaffold">
-      <Toaster toastOptions={{ className: 'rw-toast', duration: 6000 }} />
-      <header className="rw-header">
-        <h1 className="rw-heading rw-heading-primary">
-          <Link to={routes[titleTo]()} className="rw-link">
-            {title}
-          </Link>
-        </h1>
-        <Link to={routes[buttonTo]()} className="rw-button rw-button-green">
-          <div className="rw-button-icon">+</div> {buttonLabel}
-        </Link>
-      </header>
-      <main className="rw-main">{children}</main>
+    <div className="m-3 p-3">
+      {/* bg-white dark:bg-[#3b424f] */}
+      {children}
     </div>
-  )
-}
+  );
+};
 
-export default ScaffoldLayout
+export default ScaffoldLayout;
