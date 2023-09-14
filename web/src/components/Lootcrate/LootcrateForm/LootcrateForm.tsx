89aaaa8a1f3868ac36e7297ea507e383d41120df
arvidwedtstein <bassgamer03@gmail.com>
2023-09-14 13:17:28 +0200

    - input fixes

--- web/src/components/Lootcrate/LootcrateForm/LootcrateForm.tsx
+++ web/src/components/Lootcrate/LootcrateForm/LootcrateForm.tsx
@@ -8,6 +8,7 @@ import {
   CheckboxField,
   Submit,
   ColorField,
+  NumberField,
 } from "@redwoodjs/forms";
 
 import type { EditLootcrateById, UpdateLootcrateInput } from "types/graphql";
@@ -84,9 +85,7 @@ const LootcrateForm = (props: LootcrateFormProps) => {
         <div className="flex flex-col space-y-5">
           <Input name="Test" />
 
-          <Input name="Test" type="2" />
 
-          <Input name="Test" type="3" />
 
           <div className="relative max-w-sm">
             <TextField
@@ -110,13 +109,47 @@ const LootcrateForm = (props: LootcrateFormProps) => {
           <div className="rw-input-underline max-w-sm">
             <TextField
               name="name"
-              className="border-1 focus:border-pea-600 dark:focus:border-pea-500 peer block w-full appearance-none rounded-lg border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white"
+              className="peer"
               errorClassName="rw-input rw-input-error"
-              placeholder=" "
+              placeholder=""
             />
             <Label
               name="name"
-              className="peer-focus:text-pea-600 peer-focus:dark:text-pea-500 absolute top-4 left-1 z-10 origin-[0] -translate-y-4 scale-75 transform px-2 text-sm text-gray-500 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-4 peer-focus:-translate-y-4 peer-focus:scale-75 dark:text-gray-400"
+              className="peer-focus-within:text-pea-600 peer-focus-within:dark:text-pea-500 peer-placeholder-shown:top-4 peer-placeholder-shown:-translate-y-0 peer-placeholder-shown:scale-100 peer-focus-within:top-4 peer-focus-within:-translate-y-4 peer-focus-within:scale-75"
+            >
+              Name
+            </Label>
+            <FieldError name="name" className="rw-field-error" />
+          </div>
+          <div className="rw-input-underline small max-w-sm">
+            <TextAreaField
+              name="names"
+              className="peer"
+              errorClassName="rw-input rw-input-error"
+              placeholder=""
+            />
+            <Label
+              name="names"
+              className="peer-focus-within:text-pea-600 peer-focus-within:dark:text-pea-500 peer-placeholder-shown:top-4 peer-placeholder-shown:-translate-y-0 peer-placeholder-shown:scale-100 peer-focus-within:top-4 peer-focus-within:-translate-y-4 peer-focus-within:scale-75"
+
+              errorClassName="rw-label rw-label-error"
+            >
+              Names
+            </Label>
+            <FieldError name="name" className="rw-field-error" />
+            {/* <p>Helper text</p> */}
+          </div>
+          <div className="rw-input-underline max-w-sm">
+            <NumberField
+              name="name2"
+              className="peer"
+              errorClassName="rw-input rw-input-error"
+              // disabled
+              placeholder=""
+            />
+            <Label
+              name="name2"
+              className="peer-focus-within:text-pea-600 peer-focus-within:dark:text-pea-500 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-75 peer-focus-within:top-4 peer-focus-within:-translate-y-4 peer-focus-within:scale-75"
               errorClassName="rw-label rw-label-error"
             >
               Name
--- web/src/components/Util/Input/Input.tsx
+++ web/src/components/Util/Input/Input.tsx
@@ -3,66 +3,34 @@ import clsx from "clsx";
 import { useState } from "react";
 import useComponentVisible from "src/components/useComponentVisible";
 
-const Input = ({ name, type = "1" }) => {
-  const { ref, isComponentVisible, setIsComponentVisible } =
-    useComponentVisible(false);
-  const [inputValue, setInputValue] = useState("");
 
+type InputProps = {
+
+} & React.InputHTMLAttributes<HTMLInputElement>;
+const Input = ({ name, required }: InputProps) => {
   return (
-    <>
-      {type == "1" ? (
-        <div className="relative inline-flex min-w-0 flex-col border-0 p-0 align-top dark:text-zinc-300">
-          <label
-            htmlFor="input"
+    <div className="relative inline-flex flex-col">
+      <div className="relative inline-flex items-center rounded text-base font-normal leading-6 dark:text-zinc-300 text-zinc-700">
+        <fieldset
+          className="relative border m-0 min-w-[0%] overflow-hidden rounded-[inherit] border-zinc-500 px-2 text-left text-current focus:outline-0 hover:border-zinc-300">
+          <input
+            id="input"
+            className="relative peer pointer-events-auto box-content block h-[1.4375em] w-full min-w-0 border-0 bg-transparent px-3.5 py-[16.5px] placeholder:opacity-0 focus:outline-none focus:placeholder:opacity-50"
+            type="text"
+            placeholder=""
+          />
+          <legend
             className={clsx(
-              "absolute left-0 top-0 z-10 block origin-[top_left_0px] overflow-hidden overflow-ellipsis whitespace-nowrap p-0 text-base leading-[1.4375em] transition-transform",
-              {
-                "text-pea-500 pointer-events-auto max-w-[calc(133%-32px)] translate-x-3.5 -translate-y-[9px] scale-75 select-none":
-                  isComponentVisible,
-                "pointer-events-none max-w-[calc(100%-24px)] translate-x-3.5 translate-y-4 scale-100":
-                  !isComponentVisible,
-              }
+              "invisible h-0 max-w-full block w-auto whitespace-nowrap p-0 z-10 transition-all peer-placeholder-shown:[&>label]:translate-y-4 peer-placeholder-shown:[&>label]:scale-100 peer-placeholder-shown:max-w-[.01px] peer-focus-within:max-w-full peer-focus-within:[&>label]:translate-x-2.5 peer-focus-within:[&>label]:scale-75 peer-focus-within:[&>label]:-translate-y-1",
             )}
           >
-            Legend
-          </label>
-          <div
-            className="relative inline-flex cursor-text items-center rounded text-base font-normal leading-6"
-            onClick={() => setIsComponentVisible(!isComponentVisible)}
-            ref={ref}
-          >
-            <input
-              id="input"
-              className="box-content block h-[1.4375em] w-full min-w-0 border-0 bg-transparent px-3.5 py-[16.5px] placeholder:opacity-0 focus:outline-none focus:placeholder:opacity-50"
-              type="text"
-            />
-            <fieldset
-              className={clsx(
-                "pointer-events-none absolute left-0 right-0 bottom-0 -top-[5px] m-0 min-w-[0%] overflow-hidden rounded-[inherit] border-zinc-500 px-2 text-left text-current focus:outline-0 [&:hover]:border-zinc-300",
-                {
-                  "border-pea-500 border-2": isComponentVisible,
-                }
-              )}
-            >
-              <legend
-                className={clsx(
-                  "invisible float-none block h-[11px] w-auto overflow-hidden whitespace-nowrap p-0 text-xs transition-all duration-75",
-                  isComponentVisible ? "max-w-full" : "max-w-[0.01px]"
-                )}
-              >
-                <span className="visible inline-block px-1.5 opacity-0">
-                  Legend
-                </span>
-              </legend>
-            </fieldset>
-          </div>
-        </div>
-      ) : type == "2" ? (
-        <></>
-      ) : (
-        <></>
-      )}
-    </>
+            <label className="select-none transform relative transition-transform text-base z-10 inline-block origin-[top_left_0px] -translate-y-1 visible px-1.5 scale-75">
+              Legend
+            </label>
+          </legend>
+        </fieldset>
+      </div>
+    </div>
   );
 };
 
